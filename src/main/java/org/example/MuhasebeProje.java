4027c7754e2d9d89cd90fff26784d34bdaa6dacb
sarpkaya1 <sarpkaya1903@gmail.com>
2024-05-31 23:00:36 +0300

    Muhasebe departmanı maaşları düzenlendi.

--- src/main/java/org/example/MuhasebeProje.java
+++ src/main/java/org/example/MuhasebeProje.java
@@ -9,7 +9,7 @@ public class MuhasebeProje {
 
     void hesapla(String isim) {
         int normalUcret = calisilanGun * gunlukUcret;
-        int toplamUcret = (calisilanGun * gunlukUcret) + ((calisilanGun - sabitMesaiSuresi ) * mesaiUcreti);
+        int toplamUcret = (sabitMesaiSuresi * gunlukUcret) + ((calisilanGun - sabitMesaiSuresi ) * mesaiUcreti);
 
         if (calisilanGun > 25){
             System.out.println(isim + " Kişisi" + toplamUcret + " TL maaş almıştır.");
