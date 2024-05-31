93663137e30ae4f22648d3d78c2ddbb84c6dd165
sarpkaya1 <sarpkaya1903@gmail.com>
2024-05-31 19:54:15 +0300

    Çalışan maaşları düzenlendi.

---
+++ src/main/java/org/example/MuhasebeProje.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class MuhasebeProje {
+
+    int calisilanGun;
+    int gunlukUcret = 2000;
+    int mesaiUcreti = 1000;
+    int sabitMesaiSuresi = 25;
+
+    void hesapla(String isim) {
+        int normalUcret = calisilanGun * gunlukUcret;
+        int toplamUcret = (calisilanGun * gunlukUcret) + ((calisilanGun - sabitMesaiSuresi ) * mesaiUcreti);
+
+        if (calisilanGun > 25){
+            System.out.println(isim + " Kişisi" + toplamUcret + " TL maaş almıştır.");
+        }else{
+            System.out.println(isim + "Kişisi" + normalUcret + " TL maaş almıştır.");
+        }
+
+    }
+}
---
+++ src/main/java/org/example/MuhesabeTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class MuhesabeTest {
+    public static void main(String[] args) {
+        MuhasebeProje calisan1 = new MuhasebeProje();
+        MuhasebeProje calisan2 = new MuhasebeProje();
+        MuhasebeProje calisan3 = new MuhasebeProje();
+        MuhasebeProje calisan4 = new MuhasebeProje();
+
+
+        calisan1.calisilanGun = 30;
+        calisan2.calisilanGun = 25;
+        calisan3.calisilanGun = 36;
+        calisan4.calisilanGun = 32;
+
+        calisan1.hesapla("Mehmet");
+        calisan2.hesapla("Aslı");
+        calisan3.hesapla("Alev");
+        calisan4.hesapla("Cansu");
+    }
+
+}
